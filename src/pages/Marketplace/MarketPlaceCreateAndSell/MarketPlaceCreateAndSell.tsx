ce718e246ece704b448c9cc4856c769d86fca01f
Varsha Haris Lobo <lobovarsha18@gmail.com>
2022-07-16 16:05:08 +0530

    execute near nft sale

--- src/pages/Marketplace/MarketPlaceCreateAndSell/MarketPlaceCreateAndSell.tsx
+++ src/pages/Marketplace/MarketPlaceCreateAndSell/MarketPlaceCreateAndSell.tsx
@@ -1,5 +1,7 @@
 // Libs
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import * as nearAPI from 'near-api-js';
 
 // Images
 import mpWallet from "../../../assets/svgs/mpWallet.svg";
@@ -18,7 +20,78 @@ const Element = ({ element }) => {
     </div>
   );
 };
-const MarketPlaceCreateAndSell = () => {
+function MarketPlaceCreateAndSell(props: any): JSX.Element {
+
+  const [nftMarketResults, setNftMarketResults] = useState([]);
+  const [showLoader, setShowLoader] = useState(false);
+  const [assetBid, setAssetBid] = useState("");
+
+  useEffect(() => {
+    if (!showLoader) {
+      loadSaleItems();
+    }
+  }, [showLoader]);
+
+  const loadSaleItems = async () => {
+    let nftTokens = await props.near.walletConnection
+      .account()
+      .viewFunction({
+        contractId: "nft-contract.boomboom.testnet",
+        methodName: "nft_tokens",
+        args: {
+        from_index: "0",
+        limit: 64,
+      }});
+
+    let saleTokens = await props.near.walletConnection
+      .account()
+      .viewFunction({
+        contractId: "market_contract.boomboom.testnet",
+        methodName: "get_sales_by_nft_contract_id",
+        args: {
+          nft_contract_id: "nft-contract.boomboom.testnet",
+          from_index: "0",
+          limit: 64,
+        }
+        });
+
+      let sales = [];
+
+    for (let i = 0; i < nftTokens.length; i++) {
+      const { token_id } = nftTokens[i];
+      let saleToken = saleTokens.find(({ token_id: t }) => t === token_id);
+      if (saleToken !== undefined) {
+        sales[i] = Object.assign(nftTokens[i], saleToken);
+    
+      }
+    }
+    setNftMarketResults(sales);
+    setShowLoader(true);
+  };
+
+  const {
+    utils: {
+      format: { parseNearAmount},
+    },
+  } = nearAPI;
+
+  /*place an offer on a specific sale. The sale will go through as long as your deposit is greater than or equal to the list price
+  process the purchase (which will remove the sale, transfer and get the payout from the nft contract, and then distribute royalties)
+  -> Loop through nftmarketResults.map(nft, index), execute OfferPrice(nft.token_id)*/
+  const OfferPrice = async (token_id) => {
+    await props.near.walletConnection.account().functionCall({
+      contractId: "market_contract.boomboom.testnet",
+      methodName: "offer",
+      args: {
+        nft_contract_id: "nft-contract.boomboom.testnet",
+        token_id,
+      },
+      attachedDeposit: parseNearAmount(assetBid),
+      gas: "200000000000000",
+    })
+  }
+
+
   const elements = [
     {
       name: "Wallet",
