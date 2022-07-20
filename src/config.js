cf6e77251c05b6d8da385f143da4ec6075d3e8a4
Nayan96 <nnayanamin96@gmail.com>
2022-07-20 21:06:06 +0530

    config-update

--- src/config.js
+++ src/config.js
@@ -37,12 +37,12 @@ export const UNICUS_STORE =
   process.env.REACT_APP_ENV === "local"
     ? "localhost:3000"
     : process.env.REACT_APP_ENV === "development"
-    ? "marketplace.test.unicus.one"
+    ? "test.unicus.one"
     : process.env.REACT_APP_ENV === "staging"
     ? "marketplace.qa.unicus.one"
     : process.env.REACT_APP_ENV === "demo"
-    ? "marketplace.test.unicus.one"
-    : "marketplace.unicus.one";
+    ? "demo.unicus.one"
+    : "unicus.one";
 
 
 
