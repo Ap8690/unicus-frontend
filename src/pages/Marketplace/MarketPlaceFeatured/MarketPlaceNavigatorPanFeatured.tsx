8846b9c645f76e9be4a84592ee1834705a48e076
Kartik <kr.pjpt@gmail.com>
2022-09-13 18:48:34 +0530

    env

--- src/pages/Marketplace/MarketPlaceFeatured/MarketPlaceNavigatorPanFeatured.tsx
+++ src/pages/Marketplace/MarketPlaceFeatured/MarketPlaceNavigatorPanFeatured.tsx
@@ -2,58 +2,68 @@
 import { useNavigate } from "react-router-dom";
 import { getNftContractAddress } from "../../../utils/utils";
 import useExplorer from "../useExplorer";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper';
-import NftSkeletonLoader from "../../../components/Loading/SkeletonLoading/NftSkeletonLoader"
-import uuid from 'react-uuid'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
+import NftSkeletonLoader from "../../../components/Loading/SkeletonLoading/NftSkeletonLoader";
+import uuid from "react-uuid";
 
 const Element = ({ element }) => (
-  <div className="market-place-featured-element">
-    <div className="nft-image-size">
-      <img src={element.cloudinaryUrl} alt={element.heading} />
+    <div className="market-place-featured-element">
+        <div className="nft-image-size">
+            <img src={element.cloudinaryUrl} alt={element.heading} />
+        </div>
+        <h3 className="heading">
+            {element.heading?.length > 15
+                ? element.heading?.slice(0, 15)
+                : element.heading}
+        </h3>
+        <p className="text text-center">{element.name}</p>
     </div>
-    <h3 className="heading">{element.heading?.length > 15 ? element.heading?.slice(0,15) : element.heading}</h3>
-    <p className="text text-center">{element.name}</p>
-  </div> 
 );
 const MarketPlaceNavigatorPanFeatured = ({ list, currentScroll, loading }) => {
-  // const holderRef = useExplorer(currentScroll);
-  const navigate = useNavigate()
-  return (
-    loading ? (
-      <NftSkeletonLoader singleRow className='loader-margin'/>
-    ) :
-    list && list.length>0 ? <Swiper 
-      modules={[Navigation, Pagination]}  
-      className="market-place-featured-elements"
-      navigation={{
-        prevEl: '#featured-nav-left',
-        nextEl: '#featured-nav-right'
-      }}
-      breakpoints={{
-        320: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        992: {
-            slidesPerView: 3,
-        },
-    }}
-    >
-     { list.map((element: any, i: number) => (
-        <SwiperSlide
-        key={uuid()}
-          onClick={()=>navigate(`/nft/${element.chain}/${getNftContractAddress(
-              element
-            )}/${element.tokenId}`,
-      )}
+    // const holderRef = useExplorer(currentScroll);
+    const navigate = useNavigate();
+    return loading ? (
+        <NftSkeletonLoader singleRow className="loader-margin" />
+    ) : list && list.length > 0 ? (
+        <Swiper
+            modules={[Navigation, Pagination]}
+            className="market-place-featured-elements"
+            navigation={{
+                prevEl: "#featured-nav-left",
+                nextEl: "#featured-nav-right",
+            }}
+            breakpoints={{
+                320: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                992: {
+                    slidesPerView: 3,
+                },
+            }}
         >
-          <Element key={uuid()} element={element} />
-        </SwiperSlide>
-      )) }
-    </Swiper>: <div className="min-h-[300px] text-xl font-bold flex justify-center items-center">No Nfts Found</div>
-  );
+            {list.map((element: any, i: number) => (
+                <SwiperSlide
+                    key={uuid()}
+                    onClick={() =>
+                        navigate(
+                            `/nft/${element.chain}/${getNftContractAddress(
+                                element
+                            )}/${element.tokenId}`
+                        )
+                    }
+                >
+                    <Element key={uuid()} element={element} />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    ) : (
+        <div className="min-h-[300px] text-xl font-bold flex justify-center items-center">
+            No Nfts Found
+        </div>
+    );
 };
 export default MarketPlaceNavigatorPanFeatured;
--- src/web3.tsx
+++ src/web3.tsx
@@ -1,11 +1,11 @@
-import Web3 from "web3";
+import Web3 from "web3"
 
 const networkID = localStorage.getItem("networkId") ? localStorage.getItem("networkId"): 1
 
-let web3 = new Web3(Web3.givenProvider);
+let web3 = new Web3(Web3.givenProvider)
 
 export const setWeb3Provider=(provider:any)=>{
-  web3 = new Web3(provider);
+  web3 = new Web3(provider)
 }
 
-export default web3;
+export default web3
