7d27333235c9306e0a3e7a4b009b075a0aa0be9f
Kartik <kr.pjpt@gmail.com>
2022-09-15 21:52:28 +0530

    update-nft

--- src/components/404/NotFound.tsx
+++ src/components/404/NotFound.tsx
@@ -7,9 +7,9 @@ const NotFound = () => {
     <div id="notfound" className='w-full'>
 		<div className="notfound">
 			<div className="notfound-404">
-				<h1>Oops!</h1>
+				<h1>404!</h1>
 			</div>
-			<h2>404 - Page not found</h2>
+			<h2>Page not found</h2>
 			{/* <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p> */}
 			<Link className='btn' to="/home">Go To Homepage</Link>
 		</div>
