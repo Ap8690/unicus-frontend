528cbdf0f992d252e6cb2c2d38cdb72f8857efc3
Kartik <kr.pjpt@gmail.com>
2022-09-16 16:49:24 +0530

    update nav chains

--- src/components/ChainLogo/ChainLogo.tsx
+++ src/components/ChainLogo/ChainLogo.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { ChainContext } from "../../context/ChainContext";
-import {capitalize} from "../../utils/helpers";
+import { capitalize } from "../../utils/helpers";
 import Ethereum from "../../assets/blockchain-logo/ethereumLogo.svg";
 import Binance from "../../assets/blockchain-logo/binanceLogo.svg";
 import Polygon from "../../assets/blockchain-logo/polygonLogo.svg";
@@ -8,36 +8,113 @@ import Avalanche from "../../assets/blockchain-logo/avalancheLogo.svg";
 import Tron from "../../assets/blockchain-logo/tronLogo.svg";
 import Near from "../../assets/blockchain-logo/nearLogo.svg";
 import Solana from "../../assets/blockchain-logo/solanaLogo.svg";
-import Tooltip from '@mui/material/Tooltip';
+import Tooltip from "@mui/material/Tooltip";
 
 const tooltipStyle = {
-  tooltip: {
-    sx: {
-      color: "#ffff",
-      backgroundColor: "rgba(255,255,255,0.2)",
-      fontSize: "0.90em"
-    }
-  }
-}
+    tooltip: {
+        sx: {
+            color: "#ffff",
+            backgroundColor: "rgba(255,255,255,0.2)",
+            fontSize: "0.90em",
+        },
+    },
+};
 
 const ChainLogo = () => {
     const { chain } = useContext(ChainContext);
-    const chainMessage = `You are on ${capitalize(chain)} Chain`
+    const chainMessage = `You are on ${capitalize(chain)} Chain`;
     switch (chain?.toLowerCase()) {
         case "ethereum":
-            return <Tooltip componentsProps={tooltipStyle} title={chainMessage}><img className="cursor-pointer h-[30px]" src={Ethereum} alt="Ethereum" /></Tooltip>;
+            return (
+                <Tooltip componentsProps={tooltipStyle} title={chainMessage}>
+                    <div className="flex items-center justify-center">
+                        <img
+                            className="cursor-pointer h-[24px] mr-2"
+                            src={Ethereum}
+                            alt="Binance"
+                        />
+                        <div className="font-medium capitalize">{chain}</div>
+                    </div>
+                </Tooltip>
+            );
         case "binance":
-            return <Tooltip componentsProps={tooltipStyle} title={chainMessage}><img className="cursor-pointer h-[30px]" src={Binance} alt="Binance" /></Tooltip>;
+            return (
+                <Tooltip componentsProps={tooltipStyle} title={chainMessage}>
+                    <div className="flex items-center justify-center">
+                        <img
+                            className="cursor-pointer h-[24px] mr-2"
+                            src={Binance}
+                            alt="Binance"
+                        />
+                        <div className="font-medium capitalize">{chain}</div>
+                    </div>
+                </Tooltip>
+            );
         case "polygon":
-            return <Tooltip componentsProps={tooltipStyle} title={chainMessage}><img className="cursor-pointer h-[30px]" src={Polygon} alt="Polygon" /></Tooltip>;
+            return (
+                <Tooltip componentsProps={tooltipStyle} title={chainMessage}>
+                    <div className="flex items-center justify-center">
+                        <img
+                            className="cursor-pointer h-[24px] mr-2"
+                            src={Polygon}
+                            alt="Polygon"
+                        />
+                        <div className="font-medium capitalize">{chain}</div>
+                    </div>
+                </Tooltip>
+            );
         case "avalanche":
-            return <Tooltip componentsProps={tooltipStyle} title={chainMessage}><img className="cursor-pointer h-[30px]" src={Avalanche} alt="Avalanche" /></Tooltip>;
+            return (
+                <Tooltip componentsProps={tooltipStyle} title={chainMessage}>
+                    <div className="flex items-center justify-center">
+                        <img
+                            className="cursor-pointer h-[24px] mr-2"
+                            src={Avalanche}
+                            alt="Avalanche"
+                        />
+                        <div className="font-medium capitalize">{chain}</div>
+                    </div>
+                </Tooltip>
+            );
         case "tron":
-            return <Tooltip componentsProps={tooltipStyle} title={chainMessage}><img className="cursor-pointer h-[30px]" src={Tron} alt="Tron" /></Tooltip>;
+            return (
+                <Tooltip componentsProps={tooltipStyle} title={chainMessage}>
+                    <div className="flex items-center justify-center">
+                        <img
+                            className="cursor-pointer h-[24px] mr-2"
+                            src={Tron}
+                            alt="Tron"
+                        />
+                        <div className="font-medium capitalize">{chain}</div>
+                    </div>
+                </Tooltip>
+            );
         case "near":
-            return <Tooltip componentsProps={tooltipStyle} title={chainMessage}><img className="cursor-pointer h-[30px]" src={Near} alt="Near" /></Tooltip>;
+            return (
+                <Tooltip componentsProps={tooltipStyle} title={chainMessage}>
+                    <div className="flex items-center justify-center">
+                        <img
+                            className="cursor-pointer h-[24px] mr-2"
+                            src={Near}
+                            alt="Near"
+                        />
+                        <div className="font-medium capitalize">{chain}</div>
+                    </div>
+                </Tooltip>
+            );
         case "solana":
-            return <Tooltip componentsProps={tooltipStyle} title={chainMessage}><img className="cursor-pointer h-[30px]" src={Solana} alt="Solana" /></Tooltip>;
+            return (
+                <Tooltip componentsProps={tooltipStyle} title={chainMessage}>
+                    <div className="flex items-center justify-center">
+                        <img
+                            className="cursor-pointer h-[24px] mr-2"
+                            src={Solana}
+                            alt="Solana"
+                        />
+                        <div className="font-medium capitalize">{chain}</div>
+                    </div>
+                </Tooltip>
+            );
         default:
             return null;
     }
