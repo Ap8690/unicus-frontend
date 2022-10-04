3b75f6de05dfdfc1674038e586c6d40d24391679
Kartik <kr.pjpt@gmail.com>
2022-10-04 22:16:12 +0530

    loader-update

--- src/components/Loading/PageLoader.tsx
+++ src/components/Loading/PageLoader.tsx
@@ -8,7 +8,7 @@ interface PageLoaderProps {
 const PageLoader = ({ info }: PageLoaderProps) => {
     return (
             <div className="absolute bg-[#00040e] top-0 left-0 z-[100000] flex justify-center items-center h-[100vh] w-[100vw]">
-                <img src={unicusLogo} alt="" className="relative bottom-40 h-16" />
+                <img src={unicusLogo} alt="" className="relative bottom-40 h-14" />
                 <div className="Loader"></div>
             </div>
     );
